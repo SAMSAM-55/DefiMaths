190a2424a47e62064d4f176cd7ae7461f700f26b
SAMSAM55yt <132746971+SAMSAM55yt@users.noreply.github.com>
2025-03-15 21:14:06 +0100

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -3,10 +3,10 @@ import * as api from './load-api.js';
 import * as lib from './utility-lib.js';
 
 // Variables definiftions
-const quizzContainer = document.querySelector('.quizz-main');
+const quizContainer = document.querySelector('.quiz-main');
 let timerInterval;
 let animationFrameId;
-let NumberOfQuestions = 5;
+let NumberOfQuestions = api.get_num_questions();
 let IsLastAnswerCorrect = false;
 let canAnswer = true;
 let CurrentQuestion = 0;
@@ -19,9 +19,9 @@ let hasAnswered = false;
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    api.get_selected_quizz_data()
+    api.get_selected_quiz_data()
     .then(data => {
-        initializeQuizz(data);
+        initializeQuiz(data);
     })
     .catch(error => {
         console.log("File path : ", api.get_file_path())
@@ -48,21 +48,21 @@ function show_question(list, index) {
         questionElement.innerHTML = `
             <h2 class="main-question-text">Question ${index + 1}: ${question.question}</h2>
             <progress class="timer" id="timer" value="0" max="100"></progress>
-            <ul class="quizz-answer-list" correct-answer="${question.correctAnswer-1}">
+            <ul class="quiz-answer-list" correct-answer="${question.correctAnswer-1}">
                 ${shuffledOptions.map(({ option, index }) => `
-                        <button class="quizz-answer-button" style="--border-color:black;" correct-answer=${index == question.correctAnswer - 1}>
+                        <button class="quiz-answer-button" style="--border-color:black;" correct-answer=${index == question.correctAnswer - 1}>
                             <a class="answer-button-text" answer-index=${index}>${option}</a>
                             <span class="answer-indicator"><i class="fa-regular fa-circle-xmark"></i></span>
                         </button>
                 `).join('')}
             </ul>
-            <div class="quizz-utility-buttons-container">
-                <button class="quizz-utility-button" id="next-question" onclick="nextQuestion(quizzData)"><p class="quizz-utility-button-text">Next Question <i class="fa-solid fa-arrow-right"></i></p></button>
-                <button class="quizz-utility-button" id="submitt-answer" onclick="checkAnswer(globalThis.selectedAnswer, 'answer submitted')"><p class="quizz-utility-button-text">Submit Answer</p></button>
+            <div class="quiz-utility-buttons-container">
+                <button class="quiz-utility-button" id="next-question" onclick="nextQuestion(quizData)"><p class="quiz-utility-button-text">Quest Suivante <i class="fa-solid fa-arrow-right"></i></p></button>
+                <button class="quiz-utility-button" id="submitt-answer" onclick="checkAnswer(globalThis.selectedAnswer, 'answer submitted')"><p class="quiz-utility-button-text">Valider Réponse</p></button>
             </div>
-            <div class="quizz-score-container">
-                <p class="quizz-score-text">Score: <span id="score">0</span></p>
-                <p class="quizz-question-text">Question: <span id="question-index">0</span></p>    
+            <div class="quiz-score-container">
+                <p class="quiz-score-text">Score: <span id="score">0</span></p>
+                <p class="quiz-question-text">Question: <span id="question-index">0</span></p>    
             </div>
         `;
         return [question.time*1000, questionElement];
@@ -70,26 +70,35 @@ function show_question(list, index) {
 }
 
 // Fonction pour initialiser le quiz avec les données
-function initializeQuizz(data) {
-    if (!quizzContainer) {
-        console.error('Element .quizz-main non trouvé');
+function initializeQuiz(data) {
+    if (!quizContainer) {
+        console.error('Element .quiz-main non trouvé');
         return;
     }
-    quizzContainer.innerHTML = `<h1>${data.quizTitle}</h1>`;
+    quizContainer.innerHTML = `<h1>${data.quizTitle}</h1>`;
     // Add the first question
 
     const question_list = lib.pickrandomQuestions(data.questions, NumberOfQuestions);
     globalThis.question_list = question_list;
     const question_data = show_question(question_list, 0);
-    quizzContainer.appendChild(question_data[1]);
+    quizContainer.appendChild(question_data[1]);
     MathJax.typeset();
     updateScoreandQuestionLabels();
-    globalThis.quizzData = data;
+    globalThis.quizData = data;
     startTimer(question_data[0]);
 }
 
+// Test function for skipping the questions (debugging purposes only)
 
-// Fonction pour démarrer le timer
+function skipQuestions() {
+    for (let i = 0; i < NumberOfQuestions; i++) {
+        score += 1.5;
+        hasAnswered = true;
+        nextQuestion(quizData);
+    }
+}
+
+// Function for starting timer
 function startTimer(duration) {
     globalThis.currentTime
     const timerElement = document.getElementById('timer');
@@ -127,7 +136,7 @@ function startTimer(duration) {
     }, 1000);
 }
 
-// Fonction pour vérifier la réponse
+// Function for checking the answer
 function checkAnswer(button, trigger) {
     if (!button || hasAnswered) {
         show_answer();
@@ -144,7 +153,6 @@ function checkAnswer(button, trigger) {
             IsLastAnswerCorrect = true;
             answerStreak++;
             let timeRemaining = document.getElementById('timer').value/100;
-            console.log("Remaining Time : ", timeRemaining)
             score += (timeRemaining > 0.75 ? 10 : timeRemaining*10.25);
             updateScoreandQuestionLabels();
             show_answer();
@@ -167,7 +175,7 @@ function checkAnswer(button, trigger) {
 }
 
 function show_answer() {
-    Array.from(document.getElementsByClassName('quizz-answer-button')).forEach((current_btn) => {
+    Array.from(document.getElementsByClassName('quiz-answer-button')).forEach((current_btn) => {
         if (current_btn.getAttribute('correct-answer') === 'true') {
             current_btn.style.setProperty('--border-color', 'green');
             current_btn.querySelector('.answer-indicator').classList.add('answered');
@@ -178,7 +186,7 @@ function show_answer() {
 
 }
 
-// Fonction pour passer à la question suivante
+// Function for going to the next question
 function nextQuestion(data) {
 
     if (!hasAnswered) {
@@ -190,43 +198,51 @@ function nextQuestion(data) {
         CurrentQuestion++;
         const question_data = show_question(question_list, CurrentQuestion);
         const question = question_data[1];
-        quizzContainer.innerHTML = '';
-        quizzContainer.appendChild(question);
+        quizContainer.innerHTML = '';
+        quizContainer.appendChild(question);
         MathJax.typeset();
         canAnswer = true;
         startTimer(question_data[0]);
         updateScoreandQuestionLabels();
 
     } else {
-        quizzContainer.innerHTML = `<h2>End of the quiz!</h2>
-        <p class=end_score_text> Your score : <span class="main-score-container"><span class="score-integer-container"></span><span class="score-text">.</span><span class="score-decimal-digit" value=0</span></span>`;
+        quizContainer.innerHTML = `<h2>Fin du quiz!</h2>
+        <span class=end-score-text> Votre score : 
+            <span class="main-score-container">
+                <span class="score-integer-container"></span>
+                <span class="score-text">.</span>
+                <span class="score-decimal-digit" value=0></span>
+                <p class="max-score-text"> / ${NumberOfQuestions*10} </p>
+            </span>
+        </span>`;
         lib.animate_score(score, NumberOfQuestions*10);
     }
 }
 
 function updateScoreandQuestionLabels() {
-    document.getElementById('question-index').innerHTML = `${CurrentQuestion} / ${NumberOfQuestions}`;
+    document.getElementById('question-index').innerHTML = `${CurrentQuestion+1} / ${NumberOfQuestions}`;
     document.getElementById('score').innerHTML = lib.round(score, 1);
 }
 
 document.addEventListener('click', (event) => {
-    if ((event.target.classList.contains('quizz-answer-button') || event.target.classList.contains('answer-button-text')) && canAnswer) {
+    if ((event.target.classList.contains('quiz-answer-button') || event.target.classList.contains('answer-button-text')) && canAnswer) {
         selectedAnswer = event.target;
         globalThis.selectedAnswer = selectedAnswer
         clearInterval(timerInterval);
 
-        const btn = event.target.classList.contains('quizz-answer-button')
+        const btn = event.target.classList.contains('quiz-answer-button')
           ? event.target
-          : event.target.closest('.quizz-answer-button');
+          : event.target.closest('.quiz-answer-button');
 
-        document.querySelectorAll('.quizz-answer-button').forEach((button) => {
+        document.querySelectorAll('.quiz-answer-button').forEach((button) => {
             button.classList.remove('selected');
         });
         btn.classList.add('selected');
 
-    } else if ((event.target.classList.contains('quizz-answer-button') || event.target.classList.contains('answer-button-text')) && !canAnswer) {
+    } else if ((event.target.classList.contains('quiz-answer-button') || event.target.classList.contains('answer-button-text')) && !canAnswer) {
     }
 });
 
+// Declaring the checkAnswer and nextQuestion functions in  the global scope to enable calls from html buttons
 window.checkAnswer = checkAnswer;
 window.nextQuestion = nextQuestion;

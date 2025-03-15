a5324479b3b27076f7ac7860882984c729379225
SAMSAM55yt <132746971+SAMSAM55yt@users.noreply.github.com>
2025-03-15 21:13:38 +0100

    Update load-api.js

--- JS/load-api.js
+++ JS/load-api.js
@@ -1,4 +1,4 @@
-async function get_json_data(file_path) {
+export async function get_json_data(file_path) {
     const response = await fetch(file_path);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -6,15 +6,20 @@ async function get_json_data(file_path) {
     return await response.json();
 }
 
-export async function get_selected_quizz_data() {
-    return await get_json_data(localStorage.getItem('selectedQuizzFile'));
+export async function get_selected_quiz_data() {
+    return await get_json_data(localStorage.getItem('selectedQuizFile'));
 }
 
-export async function load_file(path) {
-    localStorage.setItem('selectedQuizzFile', path);
+export async function load_file(path, numQuestions) {
+    localStorage.setItem('selectedQuizFile', path);
+    localStorage.setItem('numQuestions', numQuestions);
     window.location.href = './quizz-page.html';
 }
 
 export function get_file_path() {
-    return localStorage.getItem('selectedQuizzFile');
+    return localStorage.getItem('selectedQuizFile');
+}
+
+export function get_num_questions() {
+    return localStorage.getItem('numQuestions');
 }

be50b1d860e31ca3c026dedf78eaf462dfca819f
SAMSAM55 <[sami.saubion@gmail.com]>
2025-03-29 14:51:29 +0100

    modified:   PHP/register.php

--- PHP/register.php
+++ PHP/register.php
@@ -6,8 +6,8 @@ $password = $_REQUEST['password'];
 $user_name = $_REQUEST['user-name'];
 
 if (empty("$email") || empty("$password") || empty("$user_name")) {
-    echo "<script type='text/javascript'>alert('Tous les champs ne sont pas rempli');</script>";
     header("Location: https://defimaths.net/register.html");
+    echo "<script type='text/javascript'>alert('Tous les champs ne sont pas rempli');</script>";
     exit();
 }
 
@@ -17,20 +17,23 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $create_account = "INSERT INTO `main` (user_name, email, password, progress) VALUES('$user_name', '$email', '$hashed_password', '{}')";
 
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {} else {
-    echo "<script type='text/javascript'>alert('Veuillez saisir un email valide');</script>";
     header("Location: https://defimaths.net/register.html");
+    echo "<script type='text/javascript'>alert('Veuillez saisir un email valide');</script>";
+    $conn -> close();
     exit();
 }
 
 if (preg_match("/^[a-zA-Z0-9-_]*$/", "$user_name")) {} else {
-    echo "<script type='text/javascript'>alert('Le nom d utilisateur doit seulement contenir des tirets ou des underscore et des lettres ou chiffres');</script>";
     header("Location: https://defimaths.net/register.html");
+    echo "<script type='text/javascript'>alert('Le nom d utilisateur doit seulement contenir des tirets ou des underscore et des lettres ou chiffres');</script>";
+    $conn -> close();
     exit();
 }
     
 if ($check_email_avalaible -> num_rows == 0) {} else {
-    echo "<script type='text/javascript'>alert('Un compte existe déjà pour cet email');</script>";
     header("Location: https://defimaths.net/register.html");
+    echo "<script type='text/javascript'>alert('Un compte existe déjà pour cet email');</script>";
+    $conn -> close();
     exit();
 }
 
@@ -38,8 +41,9 @@ if ($conn -> query($create_account) === TRUE) {
     header("Location: https://defimaths.net/login.html");
     echo "<script type='text/javascript'>alert('Compte créé avec succès');</script>";
 } else {
-    echo "<script type='text/javascript'>alert('Echec lors de la création du compte');</script>";
     header("Location: https://defimaths.net/register.html");
+    $conn -> close();
+    echo "<script type='text/javascript'>alert('Echec lors de la création du compte');</script>";
 }
 
 ?>

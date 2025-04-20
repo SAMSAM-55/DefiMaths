19c01d99ec67478520e99033ff593b0b401e0a30
SAMSAM55 <sami.saubion@gmail.com>
2025-04-20 12:19:20 +0200

    Modified the header username display to avoid displaying to long usernames

--- JS/update-header.js
+++ JS/update-header.js
@@ -8,12 +8,12 @@ const burger_menu = document.getElementsByClassName('burger-menu')[0]
 // It uses the user-information file to fetch the information stocked with the php session and updates the headers to display the retrieved information.
 export async function update_header() {
     const user_name_text = [document.getElementById("user-name-text"), document.getElementById("user-name-text-burger")];
-    const user_name = await user.get_user_name() || "Se connecter";
+    const user_name = await user.get_user_name() || "Se connecter"; // Ensure the username is a string
+    const displayed_user_name = (user_name.length > 15 ? user_name.slice(0, 10) + "..." : user_name); // Do not the entire user_name if it is to long (more than 15 characters). You can change this value if you want. 
     const logged_in = await user.get_is_logged_in();
-
     user_name_text.forEach(element => {
         element.setAttribute("href", (!logged_in ? "login.html" : "account.html"));
-        element.innerHTML = `<i class="fa-solid fa-user"></i> ${user_name}`;
+        element.innerHTML = `<i class="fa-solid fa-user"></i> ${displayed_user_name}`;
     })
 }
 

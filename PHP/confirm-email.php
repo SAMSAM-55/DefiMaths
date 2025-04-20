5cbd24a7b35ad8f590806a70f88373373e4edfc8
SAMSAM55 <sami.saubion@gmail.com>
2025-04-20 14:16:23 +0200

    Fixed issue with the email confirmation error handling system

--- PHP/confirm-email.php
+++ PHP/confirm-email.php
@@ -14,62 +14,73 @@ $stmt->bind_param("s", $email);
 $del_stmt = $conn->prepare($del);
 $del_stmt->bind_param("s", $email);
 
-try {
-    if ($stmt->execute()) {
-        $result = $stmt->get_result();
+if ($stmt->execute()) {
+    $result = $stmt->get_result();
 
-        if ($result->num_rows > 0 && $row = $result->fetch_assoc()) {
-            $validation_token = $row["verification_token"];
+    if ($result->num_rows > 0 && $row = $result->fetch_assoc()) {
+        $validation_token = $row["verification_token"];
 
-            if ($validation_token === $token && $validation_token !== "" && ((new DateTime())->getTimestamp() - (new DateTime($row['created_at']))->getTimestamp()) < 750) { // Verify that the token is valid and that the link was sent less than 15 minutes ago.
-                $password_hash = $row["password_hash"];
-                $username = $row["username"];
+        if ($validation_token === $token && $validation_token !== "" && ((new DateTime())->getTimestamp() - (new DateTime($row['created_at']))->getTimestamp()) < 750) { // Verify that the token is valid and that the link was sent less than 15 minutes ago.
+            $password_hash = $row["password_hash"];
+            $username = $row["username"];
 
-                $create_account_req = "INSERT INTO `main` (user_name, email, password) VALUES(?, ?, ?)";
-                $stmt = $conn->prepare($create_account_req);
-                $stmt->bind_param("sss", $username, $email, $password_hash);
+            $create_account_req = "INSERT INTO `main` (user_name, email, password) VALUES(?, ?, ?)";
+            $stmt = $conn->prepare($create_account_req);
+            $stmt->bind_param("sss", $username, $email, $password_hash);
 
-                if ($stmt->execute()) {
-                    http_response_code(200);
-                    $stmt->close();
-                    $del_stmt->execute();
-                    $del_stmt->close();
-                    $toast_type = 'success';
-                    $toast_title = "Creation du compte";
-                    $toast_message = "Votre compte chez DéfiMaths a bien été créé.";
-                    $conn->close();
-                    echo "<script type='text/javascript'>
-                        window.location.href = window.location.origin + '/login.html?toast=" . urlencode('true') . "&toast-type=" . urlencode($toast_type) . "&toast-title=" . urlencode($toast_title) . "&toast-message=" . urlencode($toast_message) . "';
-                      </script>";
-                    exit(0);
-                }
-            } else {
-                http_response_code(400);
-                $conn->close();
-                $stmt->close();
+            if ($stmt->execute()) {
+                http_response_code(200);
                 $del_stmt->execute();
                 $del_stmt->close();
-                $toast_type = 'error';
+                $stmt->close();
+                $conn->close();
+                $toast_type = 'success';
                 $toast_title = "Creation du compte";
-                $toast_message = "Le lien a expiré veuillez recommencer.";
+                $toast_message = "Votre compte chez DéfiMaths a bien été créé.";
+                echo "<script type='text/javascript'>
+                    window.location.href = window.location.origin + '/login.html?toast=" . urlencode('true') . "&toast-type=" . urlencode($toast_type) . "&toast-title=" . urlencode($toast_title) . "&toast-message=" . urlencode($toast_message) . "';
+                  </script>";
+                exit(0);
             }
         } else {
-            throw new Exception("An error occured while trying to create an account.");
+            http_response_code(400);
+            $del_stmt->execute();
+            $del_stmt->close();
+            $conn->close();
+            $stmt->close();
+            $toast_type = 'error';
+            $toast_title = "Creation du compte";
+            $toast_message = "Le lien a expiré veuillez recommencer.";
+            echo "<script type='text/javascript'>
+                    window.location.href = window.location.origin + '/register.html?toast=" . urlencode('true') . "&toast-type=" . urlencode($toast_type) . "&toast-title=" . urlencode($toast_title) . "&toast-message=" . urlencode($toast_message) . "';
+                  </script>";
+            exit(1);
         }
     } else {
-        throw new Exception("An error occured while trying to connect to database.");
+        http_response_code(400);
+        $del_stmt->execute();
+        $del_stmt->close();
+        $conn->close();
+        $stmt->close();
+        $toast_type = 'error';
+        $toast_title = "Creation du compte";
+        $toast_message = "Ce lien n'existe pas. Veuillez recommencer.";
+        echo "<script type='text/javascript'>
+                    window.location.href = window.location.origin + '/register.html?toast=" . urlencode('true') . "&toast-type=" . urlencode($toast_type) . "&toast-title=" . urlencode($toast_title) . "&toast-message=" . urlencode($toast_message) . "';
+                  </script>";
+        exit(1);
     }
-} catch (Exception $e) {
-    http_response_code(500);
-    $toast_type = 'error';
-    $toast_title = "Erreur";
-    $toast_message = "Une erreur interne est survenue. Veuillez recommencer la création de votre compte.";
-    $conn->close();
-    $stmt->close();
+} else {
+    http_response_code(403);
     $del_stmt->execute();
     $del_stmt->close();
+    $conn->close();
+    $stmt->close();
+    $toast_type = 'error';
+    $toast_title = "Creation du compte";
+    $toast_message = "Ce lien n'existe pas. Veuillez recommencer.";
     echo "<script type='text/javascript'>
-                        window.location.href = window.location.origin + '/register.html?toast=" . urlencode('true') . "&toast-type=" . urlencode($toast_type) . "&toast-title=" . urlencode($toast_title) . "&toast-message=" . urlencode($toast_message) . "';
-                      </script>";
+                    window.location.href = window.location.origin + '/register.html?toast=" . urlencode('true') . "&toast-type=" . urlencode($toast_type) . "&toast-title=" . urlencode($toast_title) . "&toast-message=" . urlencode($toast_message) . "';
+                  </script>";
     exit(1);
 }
--- README.md
+++ README.md
@@ -80,6 +80,7 @@ This project is licensed under the MIT License. See the [LICENSE](./LICENSE "MIT
 ## Contributions
 
 Contributions to this project are welcome! To contribute:
+
 1. Fork the repository.
 2. Create a new branch for your feature or bug fix.
 3. Commit your changes and push them to your fork.
